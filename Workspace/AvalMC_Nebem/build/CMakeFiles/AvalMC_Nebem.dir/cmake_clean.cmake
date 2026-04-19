6a5772b52cf660f7f7133076af7d0a17d08c91f7
JTYONG <jarrodyong03@gmail.com>
2026-04-19 18:31:52 +0800

    Update main

---
+++ Workspace/AvalMC_Nebem/build/CMakeFiles/AvalMC_Nebem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AvalMC_Nebem"
+  "AvalMC_Nebem.pdb"
+  "CMakeFiles/AvalMC_Nebem.dir/AvalMC_Nebem.C.o"
+  "CMakeFiles/AvalMC_Nebem.dir/AvalMC_Nebem.C.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AvalMC_Nebem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
